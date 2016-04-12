ecccec98d2ff83852ebfd6f2241b53c4b13d123b
Mike Farrow <contact@mikefarrow.co.uk>
2016-04-12 11:29:08 +0100

    initial commit

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+WP_CLI_CACHE_DIR="$WERCKER_CACHE_DIR/weyforth/wpcli"
+WP_CLI_CACHE_BIN="$WP_CLI_CACHE_DIR/wp"
+
+mkdir -p "$WP_CLI_CACHE_DIR"
+
+[ ! -f "$WP_CLI_CACHE_BIN" ] && \
+  curl -o "$WP_CLI_CACHE_BIN" https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+cp "$WP_CLI_CACHE_BIN" /usr/local/bin/wp
+chmod +x /usr/local/bin/wp
+
+WP_CLI_USER=${WERCKER_WP_CLI_USER:-root}
+
+[ "$WP_CLI_USER" != "root" ] && chsh -s /bin/sh "$WP_CLI_USER"
+
+su -c "wp --allow-root $WERCKER_WP_CLI_CMD" - "$WP_CLI_USER"
